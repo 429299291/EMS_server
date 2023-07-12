ae86f964cc5a199d6b87e795c2aa4df0d1acb286
朱东东 <zhudongdong@zhudongdongdeMacBook-Air.local>
2023-07-12 15:22:52 +0800

    fix: auth -avatar

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { jwtConstants } from './constants';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '6666h' },
+      signOptions: { expiresIn: '365d' },
     }),
   ],
   providers: [AuthService],
